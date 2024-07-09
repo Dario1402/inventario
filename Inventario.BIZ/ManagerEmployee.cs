9a78ea45e24ca61bf94f9b7ae113add5a53389c9
Dario1402 <dario.lopez.arg@gmail.com>
2024-07-08 21:37:26 -0300

    Creacion de las capas de administracion de empleados y movimientos de stock

---
+++ Inventario.BIZ/ManagerEmployee.cs
@@ -0,0 +1,43 @@
+﻿using Inventario.COMMON.Entidades;
+using Inventario.COMMON.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Inventario.BIZ
+{
+    public class ManagerEmployee:IManagerEmployee
+    {
+        IRepository<Employee> repository;
+        public ManagerEmployee(IRepository<Employee> repo)
+        {
+            repository = repo;
+        }
+
+        public List<Employee> ToList => repository.Read;
+
+        public bool DeleteId(string id)
+        {
+            return repository.Delete(id);
+        }
+
+        public bool Insert(Employee entity)
+        {
+            return repository.Create(entity);
+        }
+
+        public bool SelectId(string id)
+        {
+            return ToList.Where(e => e.Id == id).SingleOrDefault(); 
+
+        }
+
+        public bool Update(Employee entity)
+        {
+            return repository.Update(entity);
+        }
+    }
+}
---
+++ Inventario.BIZ/ManagerMovStock.cs
@@ -0,0 +1,42 @@
+﻿using Inventario.COMMON.Entidades;
+using Inventario.COMMON.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventario.BIZ
+{
+    public class ManagerMovStock:IManagerMovStock
+    {
+        IRepository<MovStock> repository;
+
+        public ManagerMovStock(IRepository<MovStock> repository)
+        {
+            this.repository = repository;
+        }
+
+        public List<MovStock> ToList => repository.Read;
+
+        public bool DeleteId(string id)
+        {
+            return repository.Delete(id);
+        }
+
+        public bool Insert(MovStock entity)
+        {
+            return repository.Create(entity);
+        }
+
+        public bool SelectId(string id)
+        {
+            return ToList.Where(e => e.Id == id).SingleOrDefault(); 
+        }
+
+        public bool Update(MovStock entity)
+        {
+            return repository.Update(entity);
+        }
+    }
+}
