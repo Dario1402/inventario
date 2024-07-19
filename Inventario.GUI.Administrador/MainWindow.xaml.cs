3027b8953302714c5a0b337d6f1397d59f666998
Dario1402 <dario.lopez.arg@gmail.com>
2024-07-18 22:22:16 -0300

    Empieza la creacion del GUI de admionistrador

---
+++ Inventario.GUI.Administrador/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Inventario.GUI.Administrador
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            PutButtonsEmployeeInEdition(false);
+            CleanTextBoxEmployee();
+        }
+
+        private void CleanTextBoxEmployee()
+        {
+            TbxEmployeeLastName.Clear();
+            TbxEmployeeArea.Clear();
+            TbxEmployeeId.Text = "";
+            TbxEmployeeName.Clear();
+        }
+
+        private void PutButtonsEmployeeInEdition(bool value)
+        {
+            BtnEmployeeCancel.IsEnabled = value;
+            BtnEmployeeUpdate.IsEnabled = !value;
+            BtnEmployeeDelete.IsEnabled = !value;
+            BtnEmployeeInsert.IsEnabled = value;
+            BtnEmployeeNew.IsEnabled = !value;
+        }
+
+        private void BtnEmployeeUpdate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEmployeeInsert_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEmployeeCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEmployeeDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEmployeeNew_Click(object sender, RoutedEventArgs e)
+        {
+            CleanTextBoxEmployee();
+            BtnEmployeeInsert(true);
+
+        }
+    }
+}
